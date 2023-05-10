5713ceffe44d36cd0b9a77572cf87a5f51c76a7a
all-an <all-an@hotmail.com>
2023-05-10 07:24:46 -0300

    part 1 hello nim

---
+++ 001-lesson2/lesson2.nim
@@ -0,0 +1,9 @@
+let myWholeNumber = 1
+let myName = "Allan"
+let myDecimalNumber = 1.5
+let isNimHard = false and true
+ 
+echo("my whole number: ", myWholeNumber)
+echo("my name: ", myName)
+echo("my decimal: ", myDecimalNumber)
+echo("is nim hard: ", isNimHard)
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # learn-nim
 learn-nim
+
+## options for compilation
+
+## compile 
+
+```bash
+nim c .\hello.nim
+```
+
+## run
+
+```bash
+.\hello.exe
+```
+
+## compile & run
+
+```bash
+nim c -r --verbosity:0 hello.nim
+```
