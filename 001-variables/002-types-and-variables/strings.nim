1ae19b804547cb48b6423edd5dae6be1244a54d7
all-an <all-an@hotmail.com>
2023-05-22 07:23:58 -0300

    types

---
+++ 001-variables/002-types-and-variables/strings.nim
@@ -0,0 +1,14 @@
+let aInferred = "message \nnew line"
+echo(aInferred)
+
+let aExplicit : string = "message \nnew line"
+echo(aExplicit)
+
+let a = "hello"
+let b = "world"
+
+var msg : string
+echo(msg)
+
+msg = a & " " & b
+echo(msg)
---
+++ 001-variables/002-types-and-variables/types.nim
@@ -0,0 +1,15 @@
+let myWholeNumber = 1
+let myName = "Allan"
+let myDecimalNumber = 1.5
+let isNimHard = false and true
+ 
+echo("my whole number: ", myWholeNumber, ", type: ", typeof(myWholeNumber))
+echo("my name: ", myName, ", type: ", typeof(myName))
+echo("my decimal: ", myDecimalNumber, ", type: ", typeof(myDecimalNumber))
+echo("is nim hard: ", isNimHard, ", type: ", typeof(isNimHard))
+
+let explicitTypeFalse : bool = false
+echo("explicitTypeFalse: ", explicitTypeFalse, ", type: ", typeof(explicitTypeFalse))
+
+let explicitChar : char = 'a'
+echo("explicitChar: ", explicitChar, ", type: ", typeof(explicitChar))
